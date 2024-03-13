3397edab29b1877b97375d009c6f3a1e55d55051
SanjithSenthil <109128028+SanjithSenthil@users.noreply.github.com>
2024-03-12 19:57:27 -0700

    Creaet course api

--- flask/website/views.py
+++ flask/website/views.py
@@ -1,7 +1,31 @@
-from flask import Blueprint
+from flask import Blueprint, flash, render_template, request
+from .models import Course,Requests,db
 
 views = Blueprint('views', __name__)
 
 @views.route('/')
 def home():
+    return "<h1>Home page<h1>"
+
+@views.route('/create-course', methods=['POST'])
+def createCourse():
+    if request.method == 'POST':
+        course_number = request.form.get('course_number')
+        course_name = request.form.get('course_name')
+        course_desc = request.form.get('course_desc')
+        course_limit = request.form.get('course_limit')
+        teacher_id = request.form.get('teacher_id')
+        new_course = Course(course_number=course_number, course_name=course_name, course_desc=course_desc, course_limit=course_limit, teacher_id=teacher_id)
+        db.session.add(new_course)
+        db.session.commit()
+    return ""
+
+@views.route('/create-request', methods=['POST'])
+def createRequest():
+    if request.method == 'POST':
+        username = ""
+        course = ""
+        new_request = Requests(username=username,course=course)
+        db.session.add(new_request)
+        db.session.commit()
     return "<h1>Home page<h1>"
