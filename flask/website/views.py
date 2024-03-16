c27593c0283e7e4a6faf31f62111a27f6ec9f925
SanjithSenthil <109128028+SanjithSenthil@users.noreply.github.com>
2024-03-16 14:30:49 -0700

    Linked dashboard to individual course pages

--- flask/website/views.py
+++ flask/website/views.py
@@ -4,12 +4,14 @@ from .models import Course, Request, Enrollment, db
 
 views = Blueprint('views', __name__)
 
+# Home Page (Dashboard)
 @views.route('/')
 @login_required
 def home():
     enrolled_courses = Course.query.join(Enrollment).filter(Enrollment.user_id == current_user.id).all()
     return render_template("home.html", user=current_user, enrolled_courses=enrolled_courses)
 
+# Page for Creating a New Course - Admin
 @views.route('/create-course', methods=['GET','POST'])
 def createCourse():
     if request.method == 'POST':
@@ -23,26 +25,30 @@ def createCourse():
         db.session.commit()
     return render_template("createCourse.html", user=current_user)
 
+# Creating Enrollment Request
 @views.route('/create-request', methods=['POST'])
 def createRequest():
     if request.method == 'POST':
-        user_id = request.form.get('user_id') # make login required and replace with current_user.id
+        user_id = request.form.get('user_id')
         course_id = request.form.get('course_id')
         new_request = Request(user_id=user_id, course_id=course_id)
         db.session.add(new_request)
         db.session.commit()
     return redirect(url_for('views.display_courses'))
 
+# Page for Enrolling in a New Course - Student
 @views.route('/enroll-courses')
 def display_courses():
     courses = Course.query.all()
     return render_template('enrollCourse.html', user=current_user, courses=courses)
 
+# Page for Accepting Student Request - Admin
 @views.route('/requests')
 def display_requests():
     requests = Request.query.all()
     return render_template('acceptCourse.html', requests=requests)
 
+# Accepting Enrollment Request
 @views.route('/accept-request', methods=['POST'])
 def acceptRequest():
     if request.method == 'POST':
@@ -58,6 +64,7 @@ def acceptRequest():
         
     return redirect(url_for('views.display_requests'))
 
+# Declining Enrollemnt Request
 @views.route('/decline-request', methods=['POST'])
 def declineRequest():
     if request.method == 'POST':
@@ -68,3 +75,8 @@ def declineRequest():
             db.session.delete(request_to_delete)
             db.session.commit()
     return redirect(url_for('views.display_requests'))
+
+@views.route('/course/<int:course_id>')
+def course_page(course_id):
+    course = Course.query.get(course_id)
+    return render_template('coursePage.html', course=course)
