698884bbb8ead5f2d109a539ff11427913313efd
ameykarmarkar02 <113328096+ameykarmarkar02@users.noreply.github.com>
2024-04-04 00:59:44 -0700

    Add files via upload

--- flask/tests/login_test.py
+++ flask/tests/login_test.py
@@ -12,6 +12,11 @@
 import unittest
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from flask import Flask
