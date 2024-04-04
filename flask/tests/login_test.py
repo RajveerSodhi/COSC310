2ab4d4c7dbcc2591fa2daae569f7e319e1313152
ameykarmarkar02 <113328096+ameykarmarkar02@users.noreply.github.com>
2024-04-03 21:32:29 -0700

    Add files via upload

--- flask/tests/login_test.py
+++ flask/tests/login_test.py
@@ -14,9 +14,7 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from flask import Flask
-from webdriver_manager.chrome import ChromeDriverManager
 
-driver = webdriver.Chrome(ChromeDriverManager().install())
 
 app = Flask(__name__)
 
