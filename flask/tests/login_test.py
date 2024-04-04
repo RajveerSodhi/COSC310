2b3156785551176b0b0ee6c27646667ee9568871
ameykarmarkar02 <113328096+ameykarmarkar02@users.noreply.github.com>
2024-04-03 21:39:07 -0700

    Update login_test.py

--- flask/tests/login_test.py
+++ flask/tests/login_test.py
@@ -13,6 +13,10 @@ import unittest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+
+driver = webdriver.Chrome(ChromeDriverManager().install())
+
 from flask import Flask
 
 
