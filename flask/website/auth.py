917f9869c5c834adbd47360c4de463c35cb7aa81
Reyhan-Sam <reyhansam13@gmail.com>
2024-04-04 16:38:06 -0700

    added constraint to verify password before sign up

--- flask/website/auth.py
+++ flask/website/auth.py
@@ -29,6 +29,9 @@ def signup():
     if request.method == 'POST':
         email = request.form.get('email')
         password = request.form.get('password')
+        if password != request.form.get('confirm_password'):
+            flash("Passwords don't match!", category="error")
+            return redirect(url_for('auth.signup'))
         firstName = request.form.get('firstName')
         lastName = request.form.get('lastName')
         dob = request.form.get('dob')
