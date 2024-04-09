e01533fc91dc1f81f63f1cb6656a52541a431ba8
RajveerSodhi <rajveersodhi03@gmail.com>
2024-04-09 02:02:57 -0700

    added rejected enrollments list

--- flask/website/models.py
+++ flask/website/models.py
@@ -29,7 +29,8 @@ class Request(db.Model, UserMixin):
     id=db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     course_id = db.Column(db.Integer, db.ForeignKey('courses.id'))
-    
+    status = db.Column(db.String(150))
+
 class Enrollment(db.Model, UserMixin):
     __tablename__ = 'enrollments'
     id=db.Column(db.Integer, primary_key=True)
